6a107ebaa82a3ff992232e57e32cd1ac4b4e60ec
AyushiChaudhary1206 <154445679+AyushiChaudhary1206@users.noreply.github.com>
2025-05-04 21:43:43 +0530

    Update Login.jsx

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", form);
+      const res = await axios.post("https://creatordashboardbackend-7px3.onrender.com/api/auth/login", form);
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data.user));
 
