7b5aca809cc2cca33892c9c064ea6d05ecd35806
AyushiChaudhary1206 <154445679+AyushiChaudhary1206@users.noreply.github.com>
2025-05-04 21:44:13 +0530

    Update Signup.jsx

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://creatordashboardbackend-7px3.onrender.com/api/auth/register",
         form
       );
       localStorage.setItem("token", res.data.token);
